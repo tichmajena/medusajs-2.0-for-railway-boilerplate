2bb444ae837f03f64dd322717d01f25b4ce27c30
Tich Majena <tichmajena@gmail.com>
2026-04-23 13:51:52 +0200

    products validator bug fix6

--- backend/src/api/store/products/route.ts
+++ backend/src/api/store/products/route.ts
@@ -5,7 +5,7 @@ import {
   createProductsWorkflow,
   useQueryGraphStep,
 } from "@medusajs/medusa/core-flows";
-// import { Product } from "./validators";
+import { Product } from "./validators";
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   // Retrieve default store config
@@ -14,54 +14,45 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
     fields: ["default_sales_channel_id", "default_location_id"],
   });
 
-  const body = req.validatedBody as any;
+  const { products } = req.validatedBody as { products: Product[] };
   const default_location_id = stores[0].default_location_id;
   // Create inventory item with stocked quantity at a location
 
-  const productsWithInventory = await Promise.all(
-    body?.products?.map(async (product) => {
-      const { result: inventoryItems } = await createInventoryItemsWorkflow(
-        req.scope,
-      ).run({
-        input: {
-          items: product.variants.map((variant) => {
-            return {
-              sku: variant.sku,
-              title: variant.title,
-              location_levels: [
-                {
-                  location_id: default_location_id, // your stock location ID
-                  stocked_quantity: variant.stocked_quantity || 50,
-                },
-              ],
-            };
-          }),
-        },
-      });
+  const productsWithInventory = products.map((product) => {
+    const inventoryItems = createInventoryItemsWorkflow.runAsStep({
+      input: {
+        items: product.variants.map((variant) => {
+          return {
+            sku: variant.sku,
+            title: variant.title,
+            location_levels: [
+              {
+                location_id: default_location_id, // your stock location ID
+                stocked_quantity: variant.stocked_quantity || 50,
+              },
+            ],
+          };
+        }),
+      },
+    });
 
-      // Prepare inventory item IDs to pass to the variant
-      // const inventoryItemIds = transform({ inventoryItems }, (data) => {
-      //   return data.inventoryItems.map((item) => ({
-      //     inventory_item_id: item.id,
-      //     required_quantity: 1,
-      //   }));
-      // });
-
-      const inventoryItemIds = inventoryItems.map((item) => ({
+    // Prepare inventory item IDs to pass to the variant
+    const inventoryItemIds = transform({ inventoryItems }, (data) => {
+      return data.inventoryItems.map((item) => ({
         inventory_item_id: item.id,
         required_quantity: 1,
       }));
+    });
 
-      return {
-        ...product,
-        variants: product.variants.map(
-          ({ stocked_quantity, ...variant }, i: number) => {
-            return { ...variant, inventory_items: [inventoryItemIds[i]] };
-          },
-        ),
-      };
-    }),
-  );
+    return {
+      ...product,
+      variants: product.variants.map(
+        ({ stocked_quantity, ...variant }, i: number) => {
+          return { ...variant, inventory_items: [inventoryItemIds[i]] };
+        },
+      ),
+    };
+  });
 
   const { result } = await createProductsWorkflow(req.scope).run({
     input: { products: productsWithInventory } as any,
