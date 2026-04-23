76a64525edb2c7e161c2c122585e6e4cd5427a22
Tich Majena <tichmajena@gmail.com>
2026-04-23 12:05:47 +0200

    products validator bug fix3

--- backend/src/api/store/products/route.ts
+++ backend/src/api/store/products/route.ts
@@ -1,9 +1,61 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
-import { createProductsWorkflow } from "@medusajs/medusa/core-flows";
+import { transform } from "@medusajs/framework/workflows-sdk";
+import {
+  createInventoryItemsWorkflow,
+  createProductsWorkflow,
+  useQueryGraphStep,
+} from "@medusajs/medusa/core-flows";
+import { Product } from "./validators";
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
+  // Retrieve default store config
+  const { data: stores } = useQueryGraphStep({
+    entity: "store",
+    fields: ["default_sales_channel_id", "default_location_id"],
+  });
+
+  const { products } = req.validatedBody as { products: Product[] };
+  const default_location_id = stores[0].default_location_id;
+  // Create inventory item with stocked quantity at a location
+
+  const productsWithInventory = products.map((product) => {
+    const inventoryItems = createInventoryItemsWorkflow.runAsStep({
+      input: {
+        items: product.variants.map((variant) => {
+          return {
+            sku: variant.sku,
+            title: variant.title,
+            location_levels: [
+              {
+                location_id: default_location_id, // your stock location ID
+                stocked_quantity: variant.stocked_quantity || 50,
+              },
+            ],
+          };
+        }),
+      },
+    });
+
+    // Prepare inventory item IDs to pass to the variant
+    const inventoryItemIds = transform({ inventoryItems }, (data) => {
+      return data.inventoryItems.map((item) => ({
+        inventory_item_id: item.id,
+        required_quantity: 1,
+      }));
+    });
+
+    return {
+      ...product,
+      variants: product.variants.map(
+        ({ stocked_quantity, ...variant }, i: number) => {
+          return { ...variant, inventory_items: [inventoryItemIds[i]] };
+        },
+      ),
+    };
+  });
+
   const { result } = await createProductsWorkflow(req.scope).run({
-    input: req.validatedBody as any,
+    input: { products: productsWithInventory },
   });
 
   res.send(result);
--- backend/src/api/store/products/validators.ts
+++ backend/src/api/store/products/validators.ts
@@ -1,43 +1,53 @@
 import { z } from "zod";
 
-export const PostAdminCreateProduct = z.object({
-  products: z.array(
+const variantSchema = z.object({
+  title: z.string(),
+  sku: z.string(),
+  manage_inventory: z.boolean(),
+  allow_backorder: z.boolean(),
+  stocked_quantity: z.number()?.optional(),
+  invetory_items: z
+    .array(
+      z.object({
+        inventory_item_id: z.string(),
+        required_quantity: z.string(),
+      }),
+    )
+    .optional(),
+  prices: z.array(
     z.object({
-      title: z.string(),
-      status: z.string(),
-
-      description: z.string(),
-      options: z.array(
-        z.object({
-          title: z.string(),
-          values: z.array(z.string()),
-        }),
-      ),
-      variants: z.array(
-        z.object({
-          title: z.string(),
-          sku: z.string(),
-          manage_inventory: z.boolean(),
-          allow_backorder: z.boolean(),
-          prices: z.array(
-            z.object({
-              currency_code: z.string(),
-              amount: z.number(),
-            }),
-          ),
-        }),
-      ),
-      shipping_profile_id: z.string(),
-      sales_channels: z.array(
-        z.object({
-          id: z.string(),
-        }),
-      ),
-      brand: z
-        .object({
-          id: z.string(),
-        })
-        .nullable(),
+      currency_code: z.string(),
+      amount: z.number(),
     }),
   ),
 });
+const productSchema = z.object({
+  title: z.string(),
+  status: z.enum(["published", "draft"]),
+  description: z.string(),
+  options: z.array(
+    z.object({
+      title: z.string(),
+      values: z.array(z.string()),
+    }),
+  ),
+  variants: z.array(variantSchema),
+  shipping_profile_id: z.string(),
+  sales_channels: z.array(
+    z.object({
+      id: z.string(),
+    }),
+  ),
+  brand: z
+    .object({
+      id: z.string(),
+    })
+    .nullable(),
+});
+
+export const PostAdminCreateProduct = z.object({
+  products: z.array(productSchema),
+});
+
+export type Product = z.infer<typeof productSchema>;
+export type Variant = z.infer<typeof variantSchema>;
